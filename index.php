22a38325a0af61b927769c2939034e3171f3daf8
MrMouse1 <mysh.michael@gmail.com>
2019-11-19 16:27:02 +0000

    My first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Online Portflio</title>
+    <link href="./index.css" rel="stylesheet" type="text/css"/>
+  </head>
+  <body>
+      <?php echo '<h2>Hello World! Here are all of my links! Mikhail Romanovskiy - Period 1</h2>';?> 
+    <div class="jumbotron">
+    <heading class="container">
+      <div class="row">
+        <nav class="col-1">
+        </nav>
+        <nav class="col">
+          <a href="index.html">Home</a></br>
+        </nav>
+        <nav class="col-2">
+          <a href="Romanovskiy_AboutMe.html">AboutMe</a></br>
+        </nav>
+        <nav class="col-2">
+          <a href="Romanovskiy_Resume.html">Resume</a></br>
+        </nav>
+        <nav class="col-2">
+          <a href="Romanovskiy_Projects.html">Projects</a></br>
+        </nav>
+        <nav class="col-2">
+          <a href="Romanovskiy_ContactMe.html">ContactMe</a></br>
+        </nav>
+      </div>
+    </heading>
+    </div>
+  </body>
+</html>
